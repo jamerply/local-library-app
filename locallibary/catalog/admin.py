f21038870ad4c6ccf8dac070e03caadcb2b094c2
James Mitchell <jamitchell@loc.gov>
2023-07-21 18:27:29 -0500

    initial commit

---
+++ locallibary/catalog/admin.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+from .models import Book, Author, Genre, BookInstance, Language
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    inlines = [BookInline]
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+    inlines = [BookInstanceInline]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'book', 'status', 'due_back')
+    list_filter = ('status', 'due_back')    
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+#admin.site.register(Book)
+#admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Genre)
+#admin.site.register(BookInstance)
+admin.site.register(Language)
